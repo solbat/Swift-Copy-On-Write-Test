f80c4988a57bd4056c6ab56480ef294a63c60721
kimminjun9 <kimminjun9@worksmobile.com>
2022-12-28 14:32:51 +0900

    [fix] of 파라미터명 추가하여 address(of object: UnsafeRawPointer) 로 변경

--- CopyOnWriteTest/CopyOnWriteTest/main.swift
+++ CopyOnWriteTest/CopyOnWriteTest/main.swift
@@ -11,7 +11,7 @@ struct CopyOnWriteTest {
     var count: Int = 5
 }
 
-func address(_ object: UnsafeRawPointer) -> String {
+func address(of object: UnsafeRawPointer) -> String {
     let address = Int(bitPattern: object)
     return NSString(format: "%p", address) as String
 }
@@ -22,12 +22,12 @@ var a2 = a1
 
 print("\n실험1: struct")
 print("BEFORE")
-print("address of a1: \(address(&a1))")
-print("address of a2: \(address(&a2))")
+print("address of a1: \(address(of: &a1))")
+print("address of a2: \(address(of: &a2))")
 a2.count = 10
 print("AFTER")
-print("address of a1: \(address(&a1))")
-print("address of a2: \(address(&a2))")
+print("address of a1: \(address(of: &a1))")
+print("address of a2: \(address(of: &a2))")
 
 // 실험2: Array
 var a3 = [3, 4, 5, 6]
@@ -35,12 +35,12 @@ var a4 = a3
 
 print("\n실험2: Array")
 print("BEFORE")
-print("address of a3: \(address(&a3))")
-print("address of a4: \(address(&a4))")
+print("address of a3: \(address(of: &a3))")
+print("address of a4: \(address(of: &a4))")
 a4.append(0)
 print("AFTER")
-print("address of a3: \(address(&a3))")
-print("address of a4: \(address(&a4))")
+print("address of a3: \(address(of: &a3))")
+print("address of a4: \(address(of: &a4))")
 
 // 실험3: custom COW 구현
 final class Ref<T> {
@@ -73,9 +73,9 @@ var a6 = a5
 
 print("\n실험3: custom COW 구현")
 print("BEFORE")
-print("address of a5: \(address(&a5.ref.val))")
-print("address of a6: \(address(&a6.ref.val))")
+print("address of a5: \(address(of: &a5.ref.val))")
+print("address of a6: \(address(of: &a6.ref.val))")
 a6.value.count = 10
 print("AFTER")
-print("address of a5: \(address(&a5.ref.val))")
-print("address of a6: \(address(&a6.ref.val))")
+print("address of a5: \(address(of: &a5.ref.val))")
+print("address of a6: \(address(of: &a6.ref.val))")
